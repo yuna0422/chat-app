4465cd13f4d1d43d4cf73071c5a9d829a7dd78f4
yuna0422 <yuna.hayata@smart-group.co.jp>
2026-04-30 16:06:11 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/RoomUserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomUserRepository.java
@@ -1,8 +1,14 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import  in.tech_camp.chat_app.entity.RoomUserEntity;
 
 @Mapper
 public interface  RoomUserRepository {
-    
+    @Insert("INSERT INTO room_users(user_id, room_id) VALUES(#{user.id},#{room.id})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(RoomUserEntity userRoomEntity);
 }
