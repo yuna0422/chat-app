d8e41fb5e9ec86e177fa3f11786bda89d696435f
yuna0422 <yuna.hayata@smart-group.co.jp>
2026-05-01 10:19:13 +0900

    メッセージ送信機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/MessageRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/MessageRepository.java
@@ -1,8 +1,15 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import in.tech_camp.chat_app.entity.MessageEntity;
+
 
 @Mapper
-public class MessageRepository {
-    
+public interface MessageRepository {
+    @Insert("INSERT INtO messages(content, user_id, room_id) VALUES(#{content},#{user.id},#{room.id})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(MessageEntity messageEntity);
 }
