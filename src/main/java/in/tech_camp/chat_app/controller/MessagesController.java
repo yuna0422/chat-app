e01fdac8af865f12388b0561b127422602d2fdde
yuna0422 <yuna.hayata@smart-group.co.jp>
2026-05-01 11:40:32 +0900

    空のメッセージが送信できないバリデーションの設定

--- src/main/java/in/tech_camp/chat_app/controller/MessagesController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessagesController.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,6 +21,7 @@ import in.tech_camp.chat_app.entity.MessageEntity;
 import in.tech_camp.chat_app.form.MessageForm;
 import in.tech_camp.chat_app.repository.RoomUserRepository;
 import in.tech_camp.chat_app.repository.UserRepository;
+import in.tech_camp.chat_app.validation.ValidationOrder;
 import in.tech_camp.chat_app.repository.RoomRepository;
 import in.tech_camp.chat_app.repository.MessageRepository;
 
@@ -61,10 +64,14 @@ public class MessagesController {
     }
 
     @PostMapping("/rooms/{roomId}/messages")
-    public String saveMessage(@PathVariable("roomId") Integer roomId,@ModelAttribute("messageForm") MessageForm messageForm,@AuthenticationPrincipal CustomUserDetail currentUser) {
+    public String saveMessage(@PathVariable("roomId") Integer roomId,@ModelAttribute("messageForm") @Validated(ValidationOrder.class) MessageForm messageForm,BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetail currentUser) {
+        if(bindingResult.hasErrors()){
+            return "redirect:/rooms/" + roomId + "/messages";
+        }
+
         MessageEntity message = new MessageEntity();
         message.setContent(messageForm.getContent());
-
+        
         UserEntity user = userRepository.findById(currentUser.getId());
         RoomEntity room = roomRepository.findById(roomId);
         message.setUser(user);
--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -1,9 +1,14 @@
 package in.tech_camp.chat_app.form;
 
+
+import in.tech_camp.chat_app.validation.ValidationPriority1;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MessageForm {
+
+    @NotBlank(message = "Name can't be black", groups = ValidationPriority1.class) 
     private String content;
     
 }
